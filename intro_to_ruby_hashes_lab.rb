3bf91056bcae320a70be3e0095f9adca97931b76
github username <github email address>
2019-07-29 16:58:05 -0400

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,17 @@
 def new_hash
-
+	new_hash = {}
+	new_hash
 end
 
 def actor
-
+	actor_hash = {:name => 'Dwayne The Rock Johnson'}
 end
 
 def monopoly
-	monopoly = {}
+	
+	monopoly => {
+		:railroads => {}
+	}
 
 end
 
